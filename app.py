9ce45bdfab0aeb26db28cad83a719ff2401dcf71
fl0rch <89017121+fl0rch@users.noreply.github.com>
2023-01-17 21:02:05 +0100

    Update imput num page

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.image(image)
 
 st.title('Predict average rating the books')
 rating_count = st.number_input('Enter rating_count', step=10.00)
-num_pages = st.number_input('Enter num_pages', min_value=49, step=10)
+num_pages = st.number_input('Enter num_pages', min_value=49, max_value=1500, step=10)
 authors = st.text_input('Enter author name', placeholder='Agatha Christie')
 language = st.selectbox(
     'Select language',
